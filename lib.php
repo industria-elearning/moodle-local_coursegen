2f92579051bf1be117260da5b890af2a56ffa88d
wilber-buendata <wilber@buendata.com>
2025-09-10 18:23:50 -0500

    feat: implement pluginfile handler for serving DataCurso syllabus files

---
+++ lib.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Callback implementations for DataCurso
+ *
+ * @package    local_datacurso
+ * @copyright  2025 Buendata <soluciones@buendata.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Serve the files from the local_bboard file areas.
+ *
+ * @param stdClass $course the course object
+ * @param stdClass $cm the course module object
+ * @param stdClass $context the context
+ * @param string $filearea the name of the file area
+ * @param array $args extra arguments (itemid, path)
+ * @param bool $forcedownload whether or not force download
+ * @param array $options additional options affecting the file serving
+ */
+function local_datacurso_pluginfile(
+    $course,
+    $cm,
+    $context,
+    string $filearea,
+    array $args,
+    bool $forcedownload,
+    array $options = []
+) {
+    // Make sure the user is logged.
+    require_login(null, false);
+
+    // Check the contextlevel is as expected - if your plugin is a block, this becomes CONTEXT_BLOCK, etc.
+    if ($context->contextlevel != CONTEXT_COURSE) {
+        return false;
+    }
+
+    // Check the relevant capabilities - these may vary depending on the filearea being accessed.
+    if (!has_capability('mod/datacurso:view_syllabus', $context)) {
+        return false;
+    }
+
+    // Make sure the filearea is the expected one.
+    if ($filearea !== 'syllabus') {
+        return false;
+    }
+
+    // The args is an array containing [itemid, path].
+    // Fetch the itemid from the path.
+    $itemid = array_shift($args);
+
+    // Extract the filename / filepath from the $args array.
+    $filename = array_pop($args); // The last item in the $args array.
+    if (empty($args)) {
+        // $args is empty => the path is '/'.
+        $filepath = '/';
+    } else {
+        // $args contains the remaining elements of the filepath.
+        $filepath = '/' . implode('/', $args) . '/';
+    }
+
+    // Retrieve the file from the Files API.
+    $fs = get_file_storage();
+    $file = $fs->get_file($context->id, 'local_datacurso', $filearea, $itemid, $filepath, $filename);
+    if (!$file) {
+        // The file does not exist.
+        return false;
+    }
+
+    send_stored_file($file, 0, 0, $forcedownload, $options);
+}
