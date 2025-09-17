0fec80717594f2b7b31566b2a211cad043bb3c71
wilber-buendata <wilber@buendata.com>
2025-09-17 09:26:58 -0500

    feat: add resource_parameters class to handle file uploads from URLs

---
+++ classes/mod_parameters/resource_parameters.php
@@ -0,0 +1,64 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_datacurso\mod_parameters;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . '/filelib.php');
+
+/**
+ * Class resource_parameters
+ *
+ * @package    local_datacurso
+ * @copyright  2025 Buendata <soluciones@buendata.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class resource_parameters extends base_parameters {
+    /**
+     * Returns the adjusted parameters for the module resource.
+     *
+     * @return object Adjusted parameters for the module resource.
+     */
+    public function get_parameters() {
+        global $USER, $CFG;
+        $userid = $USER->id;
+        $draftid = file_get_unused_draft_itemid();
+
+        $fileurl = $this->parameters->fileurl;
+
+        // Get file name from URL.
+        $urlparts = parse_url($fileurl);
+        $urlpath = $urlparts['path'] ?? '';
+        $filename = basename($urlpath);
+
+        // Store file in moodledata.
+        $fs = get_file_storage();
+        $context = \context_user::instance($userid);
+        $fileinfo = [
+            'contextid' => $context->id,
+            'component' => 'user',
+            'filearea' => 'draft',
+            'itemid' => $draftid,
+            'filepath' => '/',
+            'filename' => $filename,
+        ];
+        $file = $fs->create_file_from_url($fileinfo, $fileurl, null, true);
+
+        $this->parameters->files = $file->get_itemid();
+        return $this->parameters;
+    }
+}
