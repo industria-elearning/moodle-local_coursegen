c5582fe44aee15524db5cfe0cd08e4736a320b0e
wilber-buendata <wilber@buendata.com>
2025-09-17 21:41:25 -0500

    chore: bump plugin version from 2025091604 to 2025091706

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_datacurso';
 $plugin->release = '0.1.0';
-$plugin->version = 2025091604;
+$plugin->version = 2025091706;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
