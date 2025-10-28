0f5fdeb23889cd7762bd5f24658e50e537e1c8e7
wilber-buendata <wilber@buendata.com>
2025-10-27 19:12:24 -0500

    chore: bump plugin version to 2025102701

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursegen';
 $plugin->release = '0.1.0';
-$plugin->version = 2025102300;
+$plugin->version = 2025102701;
 $plugin->requires = 2024100700; // Moodle 4.5.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
